6f55f50619c7186b158fd609053a41e6314a54a4
gaobaoru-lenovo <gaobaoru2010@126.com>
2016-09-22 22:27:49 +0800

    java base

---
+++ java_test/src/com/src/Test1.java
@@ -0,0 +1,15 @@
+package com.src;
+
+import java.util.Scanner;
+
+public class Test1 {
+	public class Dog {}
+	static Scanner scan;
+	public static void main(String[] args) {
+		scan = new Scanner(System.in);
+		while(true){
+			System.out.println(scan.nextInt());
+		}
+		
+	}
+}
